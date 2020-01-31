f1dee0fe3f99d496b0121f306cde705aeb1547e4
Patrick Kranz <patrick-kranz@live.de>
2020-01-31 20:56:38 +0100

    Add switch for Fullscreen

--- Game1.cs
+++ Game1.cs
@@ -9,11 +9,16 @@ namespace monoGameTest
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private readonly Screen screen;
+
         private Texture2D stick;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            screen = new Screen(_graphics);
+
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -23,6 +28,7 @@ namespace monoGameTest
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            this.screen.Initialize();
         }
 
         protected override void LoadContent()
@@ -37,6 +43,13 @@ namespace monoGameTest
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            var keyboardstate = Keyboard.GetState();
+            var isKeyDown = keyboardstate.IsKeyDown(Keys.F);
+            if (isKeyDown)
+            {
+                screen.ToggleFullscreen();
+            }
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -44,15 +57,67 @@ namespace monoGameTest
 
         protected override void Draw(GameTime gameTime)
         {
+            this.screen.PreDraw();
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             this._spriteBatch.Begin();
-            this._spriteBatch.Draw(this.stick, Vector2.Zero,null,Color.White);
+            this._spriteBatch.Draw(this.stick, Vector2.Zero, null, Color.White);
             this._spriteBatch.End();
 
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+            this.screen.PostDraw();
+        }
+    }
+
+    public class Screen
+    {
+        private readonly GraphicsDeviceManager graphics;
+        private SpriteBatch screenBatch;
+        private RenderTarget2D canvas;
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public Screen(GraphicsDeviceManager graphics)
+        {
+            this.graphics = graphics;
+            this.Width = 1920;
+            this.Height = 1080;
+        }
+
+        public void Initialize()
+        {
+            this.screenBatch = new SpriteBatch(graphics.GraphicsDevice);
+            this.canvas = new RenderTarget2D(graphics.GraphicsDevice, Width, Height);
+
+            graphics.PreferredBackBufferWidth = Width;
+            graphics.PreferredBackBufferHeight = Height;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+        }
+
+        public void PreDraw()
+        {
+            graphics.GraphicsDevice.SetRenderTarget(canvas);
+        }
+
+        public void PostDraw()
+        {
+            graphics.GraphicsDevice.SetRenderTarget(null);
+            screenBatch.Begin();
+            screenBatch.Draw(canvas, new Rectangle(0, 0,
+                graphics.GraphicsDevice.PresentationParameters.BackBufferWidth,
+                graphics.GraphicsDevice.PresentationParameters.BackBufferHeight), Color.White);
+            screenBatch.End();
+        }
+
+        public void ToggleFullscreen()
+        {
+            graphics.PreferredBackBufferWidth = graphics.IsFullScreen ? 1280 : 1920;
+            graphics.PreferredBackBufferHeight = graphics.IsFullScreen ? 720 : 1080;
+            graphics.ToggleFullScreen();
         }
     }
 }
