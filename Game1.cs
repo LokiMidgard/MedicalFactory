370542b1dd28c0fac1cbec64a7cac41f768c9ede
Tobias Krumholz <krumholt@hochschule-trier.de>
2020-01-31 20:59:40 +0100

    simple input working

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using PaToRo_Desktop.Engine.Input;
 
 namespace monoGameTest
 {
@@ -8,6 +9,8 @@ namespace monoGameTest
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private XBoxController xBoxController;
+        private Vector2 Position;
 
         private Texture2D stick;
 
@@ -21,6 +24,8 @@ namespace monoGameTest
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            xBoxController = new XBoxController(0);
+
 
             base.Initialize();
         }
@@ -37,7 +42,8 @@ namespace monoGameTest
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            xBoxController.Update(gameTime);
+            Position.X += xBoxController.Get(Sliders.LeftStickX);
 
             base.Update(gameTime);
         }
@@ -47,7 +53,7 @@ namespace monoGameTest
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             this._spriteBatch.Begin();
-            this._spriteBatch.Draw(this.stick, Vector2.Zero,null,Color.White);
+            this._spriteBatch.Draw(this.stick, Position, null, Color.White);
             this._spriteBatch.End();
 
             // TODO: Add your drawing code here
