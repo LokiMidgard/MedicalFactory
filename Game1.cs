ee37447d204d420db0a43ff065993e66d3f4f1bb
Patrick Kranz <patrick-kranz@live.de>
2020-01-31 21:16:21 +0100

    Added background grafic

--- Game1.cs
+++ Game1.cs
@@ -12,6 +12,7 @@ namespace monoGameTest
         private readonly Screen screen;
 
         private Texture2D stick;
+        private Texture2D palaceholderBackground;
 
         public Game1()
         {
@@ -35,6 +36,7 @@ namespace monoGameTest
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             stick = Content.Load<Texture2D>("stick");
+            palaceholderBackground = Content.Load<Texture2D>("background");
             // TODO: use this.Content to load your game content here
         }
 
@@ -61,6 +63,7 @@ namespace monoGameTest
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             this._spriteBatch.Begin();
+            this._spriteBatch.Draw(this.palaceholderBackground, Vector2.Zero, null, Color.White);
             this._spriteBatch.Draw(this.stick, Vector2.Zero, null, Color.White);
             this._spriteBatch.End();
 
@@ -68,56 +71,6 @@ namespace monoGameTest
 
             base.Draw(gameTime);
             this.screen.PostDraw();
-        }
-    }
-
-    public class Screen
-    {
-        private readonly GraphicsDeviceManager graphics;
-        private SpriteBatch screenBatch;
-        private RenderTarget2D canvas;
-
-        public int Width { get; }
-        public int Height { get; }
-
-        public Screen(GraphicsDeviceManager graphics)
-        {
-            this.graphics = graphics;
-            this.Width = 1920;
-            this.Height = 1080;
-        }
-
-        public void Initialize()
-        {
-            this.screenBatch = new SpriteBatch(graphics.GraphicsDevice);
-            this.canvas = new RenderTarget2D(graphics.GraphicsDevice, Width, Height);
-
-            graphics.PreferredBackBufferWidth = Width;
-            graphics.PreferredBackBufferHeight = Height;
-            graphics.IsFullScreen = false;
-            graphics.ApplyChanges();
-        }
-
-        public void PreDraw()
-        {
-            graphics.GraphicsDevice.SetRenderTarget(canvas);
-        }
-
-        public void PostDraw()
-        {
-            graphics.GraphicsDevice.SetRenderTarget(null);
-            screenBatch.Begin();
-            screenBatch.Draw(canvas, new Rectangle(0, 0,
-                graphics.GraphicsDevice.PresentationParameters.BackBufferWidth,
-                graphics.GraphicsDevice.PresentationParameters.BackBufferHeight), Color.White);
-            screenBatch.End();
-        }
-
-        public void ToggleFullscreen()
-        {
-            graphics.PreferredBackBufferWidth = graphics.IsFullScreen ? 1280 : 1920;
-            graphics.PreferredBackBufferHeight = graphics.IsFullScreen ? 720 : 1080;
-            graphics.ToggleFullScreen();
         }
     }
 }
