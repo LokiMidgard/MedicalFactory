e5c8e7906bb206aa29807049bcae2aa9434205ca
lqdchrm <lqdchrm-code@yahoo.de>
2020-02-01 11:33:58 +0100

    added background

---
+++ GameObjects/Background.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalFactory.GameObjects
+{
+    public class Background : Sprite
+    {
+        public Background(Screen screen) : base("Hintergrund")
+        {
+            Position.X = screen.Width / 2.0f;
+            Position.Y = screen.Height / 2.0f;
+        }
+    }
+}
