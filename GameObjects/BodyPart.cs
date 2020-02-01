3cb01b8689dbef3fd503f88e5d6083c2271a49ba
Patrick Kranz <patrick-kranz@live.de>
2020-02-01 16:02:18 +0100

    Started with position

--- GameObjects/BodyPart.cs
+++ GameObjects/BodyPart.cs
@@ -15,9 +15,31 @@ namespace MedicalFactory.GameObjects
             NIERE,
             LUNGE
         }
+        public BodyPartType Type { get; }
+
         public BodyPart(BodyPartType type) : base(type.ToString())
         {
             AttachOffset = Vector2.UnitY * -48;
+            this.Type = type;
         }
+
+        public override ICanCarray AttachedTo
+        {
+            get => base.AttachedTo; set
+            {
+                base.AttachedTo = value;
+                if (value is Patient)
+                {
+                    this.AttachOffset = this.Type switch
+                    {
+                        BodyPartType.HERZ => new Vector2(0, -30),
+                        BodyPartType.LUNGE => new Vector2(0, -80),
+                        BodyPartType.NIERE => new Vector2(-20, 0),
+                        _ => throw new NotImplementedException($"Type {this.Type}")
+                    };
+                }
+            }
+        }
+
     }
 }
